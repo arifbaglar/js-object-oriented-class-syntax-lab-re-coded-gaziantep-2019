657cf9ac071f26fed4c0c2baa72db940f481eafc
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-14 09:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ class BoardMember{
   approve(){
     return "You can do that!";
   }
-  this.doCharity= function(){
+  doCharity(){
     return "I like to help people.";
-  };
-  this.releasePressStatement= function(){
+  }
+  releasePressStatement(){
     return "You will see great things from Scuber.";
-  };
-  this.sayHi= function(){
+  }
+  sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
-  };
+  }
 }
